94ea5222fe5bfe19a6344b65f731bce8925962ff
kanji95 <kanishk5991@gmail.com>
2022-10-18 16:16:26 +0530

    latest test json file

--- dataloader/create_dataset.py
+++ dataloader/create_dataset.py
@@ -4,35 +4,35 @@ import shutil
 
 basedir = "/ssd_scratch/cvit/kanishk/"
 
-old_image_dir = os.path.join(basedir, "imgs")
-new_image_dir = os.path.join(basedir, "new_images")
-
-old_mask_dir = os.path.join(basedir, "val_masks_new")
-new_mask_dir = os.path.join(basedir, "new_masks")
-
-if not os.path.exists(new_image_dir):
-    os.makedirs(new_image_dir)
-    os.makedirs(new_mask_dir)
+# old_image_dir = os.path.join(basedir, "imgs")
+# new_image_dir = os.path.join(basedir, "new_images")
+# 
+# old_mask_dir = os.path.join(basedir, "val_masks_new")
+# new_mask_dir = os.path.join(basedir, "new_masks")
+# 
+# if not os.path.exists(new_image_dir):
+#     os.makedirs(new_image_dir)
+#     os.makedirs(new_mask_dir)
 
 data = {'test':{}}
 count = 0
 for split in ["train", "val"]:
-    with open(f"/home/kanishk/vigil/autonomous_grounding/dataloader/annotation_{split}.txt", "r") as f:
+    with open(f"/home/kanishk/vigil/autonomous_grounding/dataloader/annotation_new_{split}.txt", "r") as f:
         for line in f.readlines():
 
             image_id, sentence = int(line.split()[0]), line.split()[1:]
             command = " ".join(sentence)
 
-            old_image_filename = os.path.join(old_image_dir, f'img_{split}_{image_id}.jpg')
-            old_mask_filename = os.path.join(old_mask_dir, f'gt_img_ann_{split}_{image_id}.png')
+            # old_image_filename = os.path.join(old_image_dir, f'img_{split}_{image_id}.jpg')
+            # old_mask_filename = os.path.join(old_mask_dir, f'gt_img_ann_{split}_{image_id}.png')
 
-            new_image_filename = os.path.join(new_image_dir, f'img_test_{count}.jpg')
-            new_mask_filename = os.path.join(new_mask_dir, f'gt_img_ann_test_{count}.png')
+            # new_image_filename = os.path.join(new_image_dir, f'img_test_{count}.jpg')
+            # new_mask_filename = os.path.join(new_mask_dir, f'gt_img_ann_test_{count}.png')
 
             data['test'][count] = {"command": command, "img": f'img_test_{count}.jpg'}
 
-            shutil.copy(old_image_filename, new_image_filename)
-            shutil.copy(old_mask_filename, new_mask_filename)
+            # shutil.copy(old_image_filename, new_image_filename)
+            # shutil.copy(old_mask_filename, new_mask_filename)
 
             count += 1
 
